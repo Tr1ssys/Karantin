16be0f1d4cd017ca39d70209dffa60a1fed3a781
Tr1ssys <aleksanderrr1@gmail.com>
2020-12-18 22:41:12 +0300

    third commit

--- toDo/index.php
+++ toDo/index.php
@@ -1,17 +1,38 @@
 <? include '../header.php'; ?>
-<ul class="nav flex-column">
-	<li class="nav-item">
-		<a class="nav-link active dropApple" aria-current="page" href="#">Сегодня</a>
-	</li>
-	<li class="nav-item">
-		<a class="nav-link dropApple" href="#">Неделя</a>
-	</li>
-	<li class="nav-item">
-		<a class="nav-link dropApple" href="#">Личное</a>
-	</li>
-</ul>
+<script type="text/javascript" src="script.js"></script>
+<div class="row">
+	<div class="col">
+		<ul class="nav flex-column">
+			<li class="nav-item">
+				<a class="nav-link active dropApple" aria-current="page" href="#">Сегодня</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link dropApple" href="#">Неделя</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link dropApple" href="#">Личное</a>
+			</li>
+		</ul>
+	</div>
+	<div class="col-6">
+		<div class="tableResult container" >
+			<table id="tResult" class="table table-striped table-bordered">
+				<thead>
+					<tr>
+						<th>картинка статуса</th>
+						<th >Само дело</th>
+						<th >Дата</th>
+					</tr>
+				</thead>
+				<tbody>
 
-
+				</tbody>
+			</table> 
+		</div>
+	</div>
+	<div class="col">
+	</div>
+</div>
 
 
 
