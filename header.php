f73e3eeba31c4c114d6247d121d8aff146a7a945
Tr1ssys <aleksanderrr1@gmail.com>
2020-12-18 21:07:27 +0300

    Текст коммита

--- header.php
+++ header.php
@@ -4,32 +4,32 @@
 	<title>Артиметрика</title>
 	<link rel="shortcut icon" href="/assets/pictures/sex.png" type="image/png">
 	<link rel="stylesheet" href="/css/bootstrap.css">
-	<link rel="stylesheet" href="/css/mdb.min.css">
-	<link rel="stylesheet" href="/css/datatables.min.css">
-	<link rel="stylesheet" href="/css/dataTables.bootstrap5.min.css">
+	<!-- <link rel="stylesheet" href="/css/mdb.min.css"> -->
+	<!-- <link rel="stylesheet" href="/css/datatables.min.css"> -->
+	<!-- <link rel="stylesheet" href="/css/dataTables.bootstrap5.min.css"> -->
 	<link rel="stylesheet" type="text/css" href="/css/style.css">
 	<script type="text/javascript" src="/js/mdb.min.js"></script>
 	<script type="text/javascript" src="/js/bootstrap.js"></script>
 	<script type="text/javascript" src="/js/jquery-3.5.1.min.js"></script>
 	<script type="text/javascript" src="/js/datatables.min.js"></script>
-	<script type="text/javascript" src="/js/script.js"></script>
+	<script type="text/javascript" src="/js/scriptheader.js"></script>
 
 </head>
 <body>
-	<ul class="nav nav-pills">
+	<ul class="bgApple nav nav-pills">
   <li class="nav-item">
-    <a id = "mainPage" class="nav-link active" aria-current="page" href="/">Главная страница</a>  
+    <a  id = "mainPage" class="nav-link  aApple" aria-current="page" href="/">Главная страница</a>  
   </li>
   <li class="nav-item">
-    <a id = "secondPage" class="nav-link" href="#">Что нужно сделать</a>
+    <a id = "secondPage" class="nav-link aApple" href="/toDo/">Что нужно сделать</a>
   </li>
   <li class="nav-item">
-    <a id = "thirdPage" class="nav-link" href="#">Что сделано</a>
+    <a id = "thirdPage" class="nav-link aApple" href="/done/">Что сделано</a>
   </li>
   <li class="nav-item">
-    <a id = "fourthPage" class="nav-link" href="#">Что провалено</a>
+    <a id = "fourthPage" class="nav-link aApple" href="/failedTasks/">Что провалено</a>
   </li>
   <li class="nav-item">
-    <a id = "fifthPage" class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+    <a id = "fifthPage" class="nav-link aApple" href="/archive/">Архив</a>
   </li>
 </ul>
